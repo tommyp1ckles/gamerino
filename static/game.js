dfca0822f84549d3c714951588993188cffb87a9
Robbie Speed <robert.y.speed@gmail.com>
2016-07-08 18:49:38 -0300

    clicky score

--- static/game.js
+++ static/game.js
@@ -8,6 +8,11 @@ var game = new Phaser.Game(
   { preload: preload, create: create, update: update }
 );
 
+var score = 0;
+var scoreText;
+
+var star;
+
 function preload() {
 	game.load.image('star', '/static/star.png');
 	var background = game.load.image("background", "/static/background.jpg");
@@ -15,9 +20,24 @@ function preload() {
 
 function create() {
 	var background = game.add.sprite(0, 0, 'background');
-	game.add.sprite(100, 100, 'star');
+	star = game.add.sprite(100, 100, 'star');
 	game.physics.startSystem(Phaser.Physics.ARCADE);
+  
+  star.inputEnabled = true;
+  star.events.onInputDown.add(popBalloon, this);
+  
+  scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
 }
 
 function update() {
+}
+
+function popBalloon (balloon) {
+  console.log('balloon popped');
+  // Remove the balloon
+  // balloon.kill();
+
+  //  Add and update the score
+  score += 10;
+  scoreText.text = 'Score: ' + score;
 }
