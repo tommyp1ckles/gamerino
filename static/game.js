bebcd3740e9f50b71d047d7e453a6c71c99ef5fd
Tom <tomhadlaw144@gmail.com>
2016-07-08 20:46:14 -0300

    Stuff..

--- static/game.js
+++ static/game.js
@@ -25,7 +25,9 @@ function preload() {
 	game.load.image("background", "/static/background.jpg");
 }
 
-//var graphics;
+var multiplier;
+var holyShit;
+var holyShitShown = null;
 
 function create() {
 	background = game.add.sprite(0, 0, 'background');
@@ -38,8 +40,14 @@ function create() {
 	star.inputEnabled = true;
 	star.events.onInputDown.add(popBalloon, this);
 
-	scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
-
+	scoreText = 
+		game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+	multiplier = 
+		game.add.text(300, 500, 'Multiplier: X2!', { fontSize: '42px', fill: '#FFFF00' });
+	multiplier.alpha = 0;
+	holyShit = 
+		game.add.text(200, 300, "HOLY SHIT", { fontSize: '90px', fill: '#FFFF00' });
+	holyShit.alpha = 0;
 	//graphics = game.add.graphics();
 }
 
@@ -62,7 +70,17 @@ var lastSpeedChange = Date.now();
 var SPEED_MIN = .5;
 var speed = 3;
 
+var multiplierShown = null;
 function update() {
+	if (Date.now() - holyShitShown > 1 * SECOND) {
+		console.log("????");
+		holyShitShown = Date.now()
+		holyShit.alpha = 0;
+	} 
+	if (Date.now() - multiplierShown > 5 * SECOND && 
+		multiplierShown !== null) {
+		multiplier.alpha = 0;
+	}
 	if (Date.now() - lastSpeedChange >= 20 * SECOND) {
 		lastSpeedChange = Date.now();
 		console.log("speed increase yo!: ", speed);
@@ -72,21 +90,20 @@ function update() {
 		var x = Math.random() * WIDTH;
 		//var y = Math.random() * HEIGHT;
 		var y = HEIGHT;
-		var balloon = game.add.sprite(x, y, 'balloon');
+		//var balloon = game.add.sprite(x, y, 'balloon');
 		var scale = BALLOON_SIZE_MIN + Math.random() * BALLOON_SIZE_RANGE;
-		balloon.scale.setTo(scale,scale);
 		var balloon = game.add.sprite(x, y, 'balloon_r');
 		balloon.scale.setTo(scale, scale);
 		balloon.inputEnabled = true;
 
-		var y = Math.random() * HEIGHT;
-		var balloon = game.add.sprite(x, y, 'balloon_r');;
+		//var balloon = game.add.sprite(x, y, 'balloon_r');
 		balloon.inputEnabled = true;
 		balloon.events.onInputDown.add(popBalloon, this);
 		balloon.outOfBoundsKill = true;
 		balloons.push({
 			g: balloon,
 			speed: (Math.random() * speed) + SPEED_MIN,
+			scale: scale,
 		});
 		lastBalloon = Date.now();
 
@@ -101,16 +118,24 @@ function update() {
 	for (i = 0; i < balloons.length; i++) {
 		balloons[i].g.y += -1 * balloons[i].speed;
 	}
-
+	
 	gameTimer += FRAME_DELAY;
+	
+	holyShit.angle += 15;
 }
 
 function popBalloon (balloon) {
 	console.log('balloon popped');
+	multiplierShow = Date.now();
 	// Remove the balloon
 	balloon.kill();
-
 	//  Add and update the score
-	score += 10;
+	mult = (1 / balloon.scale.x) * speed;
+	if (mult > 10) {
+		holyShit.alpha = 1;
+	}
+	multiplier.setText("Multiplier: X" + Math.floor(mult));
+	multiplier.alpha = 1;
+	score += Math.floor(10 * mult);
 	scoreText.text = 'Score: ' + score;
 }
