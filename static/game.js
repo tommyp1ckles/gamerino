a04ced371a11a5ee116220107067b53bb86ddc3c
Tom <tomhadlaw144@gmail.com>
2016-07-08 18:59:21 -0300

    :)

--- static/game.js
+++ static/game.js
@@ -13,14 +13,14 @@ function preload() {
 	game.load.image("background", "/static/background.jpg");
 }
 
-var graphics;
+//var graphics;
 
 function create() {
 	background = game.add.sprite(0, 0, 'background');
 	game.add.sprite(100, 100, 'star');
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 
-	graphics = game.add.graphics();
+	//graphics = game.add.graphics();
 }
 var WIDTH = 800;
 var HEIGHT = 600;
@@ -29,9 +29,11 @@ var lastBalloon = Date.now();
 var MILLISECOND = 1000;
 var BALLOON_INTERVAL = 1 * MILLISECOND;
 var balloons = [];
+var 
 function update() {
 	if (Date.now() - lastBalloon >= BALLOON_INTERVAL) {
-		graphcis = game.add.graphics();
+		graphics = game.add.graphics();
+		graphics.outOfBoundsKill = true;
 		graphics.lineStyle(0);
 		graphics.beginFill(0xFFFF0B, 0.5);
 		var x = Math.random() * WIDTH;
@@ -40,9 +42,12 @@ function update() {
 		graphics.endFill();
 		balloons.push({
 			g: graphics,
-			locX: x,
-			locY: y,
 		});
 		lastBalloon = Date.now();
+	}
+
+	var i;
+	for (i = 0; i < balloons.length; i++) {
+		balloons[i].g.y += -1;
 	}
 }
