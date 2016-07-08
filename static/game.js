81e701eb8b865444e38fc11dd32e8d0df05c75da
tommy144 <tomhadlaw144@gmail.com>
2016-07-07 22:02:34 -0300

    Added stuff.

---
+++ static/game.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+
+var game = new Phaser.Game(
+  1920,
+  1080,
+  Phaser.AUTO,
+  '',
+  { preload: preload, create: create, update: update }
+);
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
