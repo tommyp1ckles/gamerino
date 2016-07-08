5fb98a5d32a7098aa0bb5dd432678aea4afcdc3b
jerwheaton <jerwheaton@gmail.com>
2016-07-08 19:44:06 -0300

    Moving BG

--- static/game.js
+++ static/game.js
@@ -4,47 +4,48 @@ var WIDTH = 800;
 var HEIGHT = 600;
 
 var game = new Phaser.Game(
-  WIDTH,
-  HEIGHT,
-  Phaser.AUTO,
-  '',
-  { preload: preload, create: create, update: update }
+	WIDTH,
+	HEIGHT,
+	Phaser.AUTO,
+	'',
+	{ preload: preload, create: create, update: update }
 );
 var background;
 var score = 0;
 var scoreText;
 
-var star;
-
 function preload() {
-	game.load.image('star', '/static/star.png');
-	game.load.image("background", "/static/background.jpg");
+	game.load.image("background", "static/background.jpg");
 }
 
 //var graphics;
 
 function create() {
 	background = game.add.sprite(0, 0, 'background');
-	star = game.add.sprite(100, 100, 'star');
+	background.anchor.setTo(0, 0.925);
+
+	//background.width = game.world.width;
 	game.physics.startSystem(Phaser.Physics.ARCADE);
-  
-  star.inputEnabled = true;
-  star.events.onInputDown.add(popBalloon, this);
-  
-  scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+
+	scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
 
 	//graphics = game.add.graphics();
 }
 
+const FRAME_DELAY = 16;
+var gameTimer = 0;
+
+const BG_SPEED = 0.25;
 var lastBalloon = Date.now();
 var MILLISECOND = 1000;
 var BALLOON_INTERVAL = 1 * MILLISECOND;
+const BG_DELAY = 5 * MILLISECOND;
 var balloons = [];
 function update() {
 	if (Date.now() - lastBalloon >= BALLOON_INTERVAL) {
 		var balloon = game.add.graphics();
-    balloon.inputEnabled = true;
-    balloon.events.onInputDown.add(popBalloon, this);
+		balloon.inputEnabled = true;
+		balloon.events.onInputDown.add(popBalloon, this);
 		balloon.outOfBoundsKill = true;
 		balloon.lineStyle(0);
 		balloon.beginFill(0xFFFF0B, 0.5);
@@ -56,21 +57,32 @@ function update() {
 			g: balloon,
 		});
 		lastBalloon = Date.now();
-    
+
+	}
+
+	// Update bg
+	if (gameTimer >= BG_DELAY && background.y < background.height - game.world.height) {
+		background.y += BG_SPEED;
+	} else {
+		console.log("POS: ", background.y);
+		console.log("OK: ", background.height);
+		console.log("DEBUG: ", background.height * (0.925));
 	}
 
 	var i;
 	for (i = 0; i < balloons.length; i++) {
 		balloons[i].g.y += -1;
 	}
+
+	gameTimer += FRAME_DELAY;
 }
 
 function popBalloon (balloon) {
-  console.log('balloon popped');
-  // Remove the balloon
-  // balloon.kill();
+	console.log('balloon popped');
+	// Remove the balloon
+	// balloon.kill();
 
-  //  Add and update the score
-  score += 10;
-  scoreText.text = 'Score: ' + score;
+	//  Add and update the score
+	score += 10;
+	scoreText.text = 'Score: ' + score;
 }
