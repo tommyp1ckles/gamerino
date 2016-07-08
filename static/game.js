cd5cfbb60a0835833155e173278bdf6bcce8d8b5
Tom <tomhadlaw144@gmail.com>
2016-07-08 18:07:21 -0300

    LOL

--- static/game.js
+++ static/game.js
@@ -1,17 +1,22 @@
 console.log("hello world");
 
 var game = new Phaser.Game(
-  1920,
-  1080,
+  800,
+  600,
   Phaser.AUTO,
   '',
   { preload: preload, create: create, update: update }
 );
 
 function preload() {
+	game.load.image('star', '/static/star.png');
+	var background = game.load.image("background", "/static/background.jpg");
 }
 
 function create() {
+	var background = game.add.sprite(0, 0, 'background');
+	game.add.sprite(100, 100, 'star');
+	game.physics.startSystem(Phaser.Physics.ARCADE);
 }
 
 function update() {
